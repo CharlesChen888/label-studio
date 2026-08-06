93d60cc32fd0789a105be6987862e314efa3632e
chentianjie <TJ_Chen@outlook.com>
2026-08-07 03:48:00 +0800

    Add a start script for dev.

---
+++ start.sh
@@ -0,0 +1,8 @@
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+source .venv/bin/activate
+cd web
+bun run build
+cd ..
+python label_studio/manage.py migrate
+python label_studio/manage.py collectstatic
